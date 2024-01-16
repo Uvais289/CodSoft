e8da29376dede70877c729bb3ea7ea4ac1f38d7f
Uvais <sheikhuvais289@gmail.com>
2024-01-16 19:49:16 +0530

    Calculator-App

---
+++ calculator-app/src/Calculator.js
@@ -0,0 +1,41 @@
+// Calculator.js
+import React, { useState } from 'react';
+ 
+
+const Calculator = () => {
+  const [input, setInput] = useState('');
+  const [result, setResult] = useState('');
+
+  const handleClick = (value) => {
+    if (value === '=') {
+      try {
+        setResult(eval(input).toString());
+      } catch (error) {
+        setResult('Error');
+      }
+    } else if (value === 'C') {
+      setInput('');
+      setResult('');
+    } else {
+      setInput(input + value);
+    }
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">
+        <div className="input">{input}</div>
+        <div className="result">{result}</div>
+      </div>
+      <div className="buttons">
+        {['7', '8', '9', '+', '4', '5', '6', '-', '1', '2', '3', '*', '0', '.', '/', 'C', '='].map((btn) => (
+          <button key={btn} onClick={() => handleClick(btn)}>
+            {btn}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
